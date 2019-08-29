d957de2a9b84a647a9dac63c2c9f96239cf7cd99
faikhanx <54686561+faikhanx@users.noreply.github.com>
2019-08-29 16:43:18 -0400

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Welcome {
+	public static void main (String[] args) {
+		
+		String welcome = "Welcome!";
+		System.out.print(welcome);
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("What is your name? I'm a program");
+		
+		String name = input.nextLine();
+		
+		System.out.println("Nice to meet you, " + name + "!");
+		
+		
+		
+		
+		
+	}
+
+}
